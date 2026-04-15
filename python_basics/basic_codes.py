5e5b09134057970b676236bb86cece46607929d3
Lahiru Madhusanka <lahirumadushanka82887721@gmail.com>
2026-04-15 23:24:11 +0530

    Add additional print statements for name and capitalization examples

--- python_basics/basic_codes.py
+++ python_basics/basic_codes.py
@@ -6,3 +6,15 @@ print ("I am 22 years old")
 print ("I am a DevOps Enthusiast")
 
 
+#This is the second command_______
+
+name = "Lahiru Avishka Madhusanka"
+Name = "Lahiru Avishka Madhusanka"
+print (name)
+print (Name)
+
+
+#This is the third command_______
+
+
+
