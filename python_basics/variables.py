3f06111fd9a8c3f84cc77784716754ab4020a34b
Lahiru Madhusanka <lahirumadushanka82887721@gmail.com>
2026-04-25 16:12:01 +0530

    Add initial implementation of variable definitions and printing examples

---
+++ python_basics/variables.py
@@ -0,0 +1,57 @@
+#variables
+#variables are used to store data in a program. They are like containers that hold values.
+#In Python, you can create a variable by assigning a value to it using the equals sign (=). 
+#For example:
+a =100
+
+age =24
+height =1.63
+name="lahiru"
+is_student=True
+#In this example, we have created five variables: a, age, height, name, and is_student.
+#The variable a is assigned the value 100, age is assigned 24, height is assigned 1.63, name is assigned "lahiru", and is_student is assigned True.
+
+#printing the variables
+print(a)
+print ("age :", age)
+print ("height :",height)
+print ("name :", name)
+print ("is student :", is_student)
+
+first_name = "Lahiru"
+last_name = "Madhusanka"
+full_name = first_name + " " + last_name
+print("full name :", full_name)
+
+
+
+age = 25
+print (type(age))
+print(age)
+print("age",age)
+
+g_name = "Dil"
+print("g_name is :" \
+    ,g_name)
+
+height =5.22
+print("height is :", height)
+print("type of height is :", type(height))
+
+
+
+#convention of variable names
+#In Python, there are some conventions for naming variables.
+#Variable names should be descriptive and use lowercase letters with words separated by underscores (snake_case).
+#For example:
+my_age = 25
+my_height = 1.63
+my_name = "lahiru"
+
+#converting data types
+
+height=5.11
+#type(height)
+float(int(height))
+
+
