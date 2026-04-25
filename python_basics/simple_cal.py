457b372d26f7d27cd55c2b40ed99670afdf513a3
Lahiru Madhusanka <lahirumadushanka82887721@gmail.com>
2026-04-25 16:40:49 +0530

    Add new simple calculator implementation with basic arithmetic operations

---
+++ python_basics/simple_cal.py
@@ -0,0 +1,20 @@
+#simple calculator
+
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number :"))
+
+#addition
+add = num1 + num2
+print("Addition :", add)
+#subtraction
+sub = num1- num2
+print("Subtraction :", sub)
+
+#multiplication
+mul = num1 *num2 
+print("Multiplication :", mul)
+
+#division
+div = num1/num2
+print("Division :", div)
+
--- python_basics/variables.py
+++ python_basics/variables.py
@@ -77,4 +77,10 @@ print(var,(type(var)))
 var = "Hello"
 print(var,(type(var)))
 
+var = 3.14
+print (var, type(var))
+
+
+
+
 
