663cbfdf39ae29bbb4d82fe65a3d4b44bab03376
Lahiru Madhusanka <lahirumadushanka82887721@gmail.com>
2026-05-30 01:12:17 +0530

    Add arithmetic and comparison operators examples in operators.py

---
+++ python_basics/operators.py
@@ -0,0 +1,34 @@
+#hey
+print("Hello, World!")
+
+#Arithmetic Operators
+a = 10
+b =5
+print (a+b)
+
+
+add_result = a + b
+print (add_result)
+
+sub_result = a-b
+print(sub_result)
+
+mul_result = a*b
+print(mul_result)
+
+div_result = a/b
+print(div_result)
+
+floor_div_result = a//b
+print(floor_div_result)
+
+mod_result = a%b
+print(mod_result)
+
+#Comparison Operators
+print (a > b)
+print (a < b)
+print (a == b)
+print (a != b)
+print (a >= b)
+print (a <= b)
